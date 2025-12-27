11ae4d376b531d450332c3f930737c3745f09f6a
Thanh Long <occurmikey@gmail.com>
2025-12-28 00:28:54 +0700

    feat(db): add Store, Cabin, Product, ProductCategory entities

---
+++ ToyCabin.Domain/Entities/ProductCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyCabin.Domain.Entities
+{
+	public class ProductCategory
+	{
+		public Guid Id { get; set; }
+		public string Code { get; set; } = string.Empty; // DOG-ROBOT
+		public string Name { get; set; } = string.Empty;
+		public string? Description { get; set; }	
+		public bool IsActive { get; set; } = true;
+		public DateTime CreatedAt { get; set; }
+		public DateTime? UpdatedAt { get; set; }
+		public virtual ICollection<Product> Products { get; set; } = new List<Product>();	
+	}
+}
