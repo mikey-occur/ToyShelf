1d99076a80142b78dc120228b98c198e4e8b3ba5
Thanh Long <occurmikey@gmail.com>
2026-02-24 14:07:08 +0700

    feat(entity): add city entity

---
+++ ToyShelf.Infrastructure/Migrations/20260224065555_AddCity.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToyShelf.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CityId",
+                table: "Warehouses",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Code = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    Name = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Warehouses_CityId",
+                table: "Warehouses",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_Code",
+                table: "Cities",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_Name",
+                table: "Cities",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Warehouse_City",
+                table: "Warehouses",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Warehouse_City",
+                table: "Warehouses");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Warehouses_CityId",
+                table: "Warehouses");
+
+            migrationBuilder.DropColumn(
+                name: "CityId",
+                table: "Warehouses");
+        }
+    }
+}
