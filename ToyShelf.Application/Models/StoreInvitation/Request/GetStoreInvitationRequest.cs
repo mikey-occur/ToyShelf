f3391e5b7f46bb0e8ffe7b82d0524b793c78a60e
Thanh Long <occurmikey@gmail.com>
2026-03-05 23:59:14 +0700

    feat(store-invitation): add API to get invitations by partner, store and status

---
+++ ToyShelf.Application/Models/StoreInvitation/Request/GetStoreInvitationRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyShelf.Domain.Entities;
+
+namespace ToyShelf.Application.Models.StoreInvitation.Request
+{
+	public class GetStoreInvitationRequest
+	{
+		public InvitationStatus? Status { get; set; }
+		public Guid? StoreId { get; set; }
+		public Guid? PartnerId { get; set; }
+	}
+}
