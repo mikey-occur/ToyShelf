e6a855cc8a8bcb67e27ba07bec12d535ece02746
Thanh Long <occurmikey@gmail.com>
2026-03-08 17:29:26 +0700

    feat(warehouse): add inventory location when creating warehouse

---
+++ ToyShelf.Infrastructure/Repositories/InventoryLocationRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyShelf.Domain.Entities;
+using ToyShelf.Domain.IRepositories;
+using ToyShelf.Infrastructure.Context;
+
+namespace ToyShelf.Infrastructure.Repositories
+{
+	public class InventoryLocationRepository : GenericRepository<InventoryLocation>, IInventoryLocationRepository
+	{
+		public InventoryLocationRepository(ToyShelfDbContext context) : base(context) { }
+		public async Task<InventoryLocation?> GetByWarehouseIdAsync(Guid warehouseId)
+		{
+			return await _context.InventoryLocations
+				.FirstOrDefaultAsync(x => x.WarehouseId == warehouseId);
+		}
+	}
+}
