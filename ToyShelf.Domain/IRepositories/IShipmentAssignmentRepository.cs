10466f7070dc90537d4474d81260ffd55bfb436b
Thanh Long <occurmikey@gmail.com>
2026-03-16 01:55:24 +0700

    feat(shipment-assignment): implement create, accept and reject assignment flow

---
+++ ToyShelf.Domain/IRepositories/IShipmentAssignmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyShelf.Domain.Entities;
+
+namespace ToyShelf.Domain.IRepositories
+{
+	public interface IShipmentAssignmentRepository : IGenericRepository<ShipmentAssignment>
+	{
+		Task<IEnumerable<ShipmentAssignment>> GetByShipperIdWithOrderAsync(Guid shipperId);
+		Task<ShipmentAssignment?> GetByIdWithDetailsAsync(Guid id);
+	}
+}
