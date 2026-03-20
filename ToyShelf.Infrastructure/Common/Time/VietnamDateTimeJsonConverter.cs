b0fad4b86a96165ec650bfc00f392920335a0564
conghuy113 <conghuy.fptu@gmail.com>
2026-03-20 13:58:01 +0700

    refactor(project): add VietnamTimeZoneResolver

--- ToyShelf.Infrastructure/Common/Time/VietnamDateTimeJsonConverter.cs
+++ ToyShelf.Infrastructure/Common/Time/VietnamDateTimeJsonConverter.cs
@@ -10,37 +10,8 @@ namespace ToyShelf.Infrastructure.Common.Time
 {
 	public class VietnamDateTimeJsonConverter : JsonConverter<DateTime>
 	{
-		private static readonly string[] VietnamTimeZoneIds =
-		{
-			"SE Asia Standard Time",
-			"Asia/Ho_Chi_Minh"
-		};
-
 		private static readonly TimeZoneInfo VietnamTimeZone =
-			ResolveVietnamTimeZone();
-
-		private static TimeZoneInfo ResolveVietnamTimeZone()
-		{
-			foreach (var id in VietnamTimeZoneIds)
-			{
-				try
-				{
-					return TimeZoneInfo.FindSystemTimeZoneById(id);
-				}
-				catch (TimeZoneNotFoundException)
-				{
-					// Try next ID.
-				}
-				catch (InvalidTimeZoneException)
-				{
-					// Try next ID.
-				}
-			}
-
-			Console.Error.WriteLine(
-				"[VietnamDateTimeJsonConverter] Vietnam time zone not found. Falling back to UTC.");
-			return TimeZoneInfo.Utc;
-		}
+			VietnamTimeZoneResolver.VietnamTimeZone;
 
 		public override DateTime Read(
 			ref Utf8JsonReader reader,
