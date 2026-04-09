e218d1b72d2971f05e8d4c3b429e3450f4d1e761
hassanPQT <phamquoctuan2112@gmail.com>
2026-04-09 15:15:46 +0700

    feat(commissionhistory):get commission history

---
+++ ToyShelf.Application/Services/CommissionHistoryService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyShelf.Application.IServices;
+using ToyShelf.Application.Models.CommissionHistory.Response;
+using ToyShelf.Domain.IRepositories;
+
+namespace ToyShelf.Application.Services
+{
+	public class CommissionHistoryService : ICommissionHistoryService
+	{
+		private readonly ICommissionHistoryRepsitory _commissionRepo;
+
+		public CommissionHistoryService(ICommissionHistoryRepsitory commissionRepo)
+		{
+			_commissionRepo = commissionRepo;
+		}
+
+		public async Task<(IEnumerable<CommissionHistoryResponse> Items, int TotalCount)> GetHistoriesPaginatedAsync(
+			int pageNumber = 1,
+			int pageSize = 10,
+			Guid? partnerId = null,
+			string? searchItem = null)
+		{
+
+			var (entities, totalCount) = await _commissionRepo.GetHistoriesPaginatedAsync(pageNumber, pageSize, partnerId, searchItem);
+
+			var items = entities.Select(c => new CommissionHistoryResponse
+			{
+				Id = c.Id,
+				OrderItemId = c.OrderItemId,
+				AppliedRate = c.AppliedRate,
+				CommissionAmount = c.CommissionAmount,
+				CreatedAt = c.CreatedAt,
+				Quantity = c.OrderItem.Quantity,
+				OrderCode = c.OrderItem.Order.OrderCode,
+				PaymentMethod = c.OrderItem.Order.PaymentMethod,
+				OrderDate = c.OrderItem.Order.CreatedAt
+			}).ToList(); 
+
+			return (items, totalCount);
+		}
+	}
+}
