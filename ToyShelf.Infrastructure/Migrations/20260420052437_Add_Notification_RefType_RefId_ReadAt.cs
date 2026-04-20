2ac90888843d71fa3d9449e31598e937ea2f76d4
Thanh Long <occurmikey@gmail.com>
2026-04-20 12:26:09 +0700

    feat(notification): Add_Notification_RefType_RefId_ReadAt

---
+++ ToyShelf.Infrastructure/Migrations/20260420052437_Add_Notification_RefType_RefId_ReadAt.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToyShelf.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Notification_RefType_RefId_ReadAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReadAt",
+                table: "Notifications",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RefId",
+                table: "Notifications",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefType",
+                table: "Notifications",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReadAt",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "RefId",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "RefType",
+                table: "Notifications");
+        }
+    }
+}
