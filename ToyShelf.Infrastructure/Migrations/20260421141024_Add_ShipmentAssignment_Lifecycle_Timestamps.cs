a7c4ea7c5a751755a68be39040add7a883411569
Thanh Long <occurmikey@gmail.com>
2026-04-21 21:11:12 +0700

    chore: add migration for ShipmentAssignment lifecycle timestamps

---
+++ ToyShelf.Infrastructure/Migrations/20260421141024_Add_ShipmentAssignment_Lifecycle_Timestamps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToyShelf.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_ShipmentAssignment_Lifecycle_Timestamps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CompletedAt",
+                table: "ShipmentAssignments",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "InProgressAt",
+                table: "ShipmentAssignments",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompletedAt",
+                table: "ShipmentAssignments");
+
+            migrationBuilder.DropColumn(
+                name: "InProgressAt",
+                table: "ShipmentAssignments");
+        }
+    }
+}
