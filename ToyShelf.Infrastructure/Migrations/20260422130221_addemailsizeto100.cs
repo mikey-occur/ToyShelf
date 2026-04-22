789aa45fe77974ad7d5bc41cdefcbb57cd2d6575
hassanPQT <phamquoctuan2112@gmail.com>
2026-04-22 20:04:36 +0700

    fix(db): add size for email to 100

---
+++ ToyShelf.Infrastructure/Migrations/20260422130221_addemailsizeto100.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToyShelf.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addemailsizeto100 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerEmail",
+                table: "Orders",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerEmail",
+                table: "Orders",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
