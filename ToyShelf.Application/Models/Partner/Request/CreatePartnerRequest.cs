69f079c4c94f0f9f345f08ca2e2be03d412d0790
phamquoctuan <phamquoctuan2112@gmail.com>
2026-04-28 21:49:53 +0700

    fix(partner): fix partner code

--- ToyShelf.Application/Models/Partner/Request/CreatePartnerRequest.cs
+++ ToyShelf.Application/Models/Partner/Request/CreatePartnerRequest.cs
@@ -10,7 +10,10 @@ namespace ToyShelf.Application.Models.Partner.Request
 	{
 		public Guid PartnerTierId { get; set; }
 		public string CompanyName { get; set; } = string.Empty;
-		public string Address { get; set; } = string.Empty;
+        public string? BankName { get; set; }
+        public string? BankAccountNumber { get; set; }
+        public string? BankAccountName { get; set; }
+        public string Address { get; set; } = string.Empty;
 		public double? Latitude { get; set; }
 		public double? Longitude { get; set; }
 		public Guid? CommissionTableId { get; set; }
